97afab27a8fef0c89179a3336e6d9990bf703862
Oren Griffin <oren.griffin@gmail.com>
2015-04-16 17:13:38 +0300

    intial comit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var multer = require('multer');
+var fs = require('fs');
+
+
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename;
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path);
+        //done=true;
+    }
+}));
+app.use(bodyParser.json());
+
+app.post('/upload', function (req, res) {
+   console.log ('a');
+    console.log(__dirname);
+    res.end();
+});
