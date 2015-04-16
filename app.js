13d9137c9a191bd3c836efda1ea1e76ce01ccc5b
Oren Griffin <oren.griffin@gmail.com>
2015-04-16 18:10:40 +0300

    intial comit

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
+
     console.log(__dirname);
 
 });
