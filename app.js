e420f7f6b324848b66fec36e447e3483bfe10c24
Oren Griffin <oren.griffin@gmail.com>
2015-04-19 17:21:27 +0300

    intial comit

--- app.js
+++ app.js
@@ -17,16 +17,18 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var AWS_SECRET_KEY = null,
-    AWS_ACCESS_KEY = null,
-    S3_BUCKET;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY,
+    AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY,
+    S3_BUCKET = process.env.S3_BUCKET;
 
 fs.readFile(__dirname + '/credentials.json', function (err, data) {
     if (data)
+    {
         var credentials = JSON.parse(data.toString());
-    AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY || credentials.access_key;
-    AWS_SECRET_KEY = process.env.AWS_SECRET_KEY || credentials.secret_key;
-    S3_BUCKET = process.env.S3_BUCKET || credentials.bucket;
+        AWS_ACCESS_KEY = credentials.access_key;
+        AWS_SECRET_KEY =  credentials.secret_key;
+        S3_BUCKET = credentials.bucket;
+    }
 });
 
 app.set('port', process.env.PORT || 3020);
