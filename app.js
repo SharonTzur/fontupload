c4321cd36feac71cc1d44ca53470dd1abad10623
Oren Griffin <oren.griffin@gmail.com>
2015-04-19 17:14:21 +0300

    intial comit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var multer = require('multer');
 var fs = require('fs');
 var aws = require('aws-sdk');
+var zlib = require('zlib');
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,17 +13,23 @@ app.all('*', function (req, res, next) {
     next();
 });
 
-
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+var AWS_SECRET_KEY = null,
+    AWS_ACCESS_KEY = null,
+    S3_BUCKET;
 
-app.set('port', process.env.PORT || 3010);
+fs.readFile(__dirname + '/credentials.json', function (err, data) {
+    var credentials = JSON.parse(data.toString());
+    AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY || credentials.access_key;
+    AWS_SECRET_KEY = process.env.AWS_SECRET_KEY || credentials.secret_key;
+    S3_BUCKET = process.env.S3_BUCKET || credentials.bucket;
+});
 
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = process.env.S3_BUCKET;
+app.set('port', process.env.PORT || 3020);
+
 
 var server = app.listen(app.get('port'), function () {
 
@@ -31,15 +38,12 @@ var server = app.listen(app.get('port'), function () {
 
     console.log('Example app listening at http://%s:%s', host, port);
 
-    console.log(AWS_ACCESS_KEY);
-    console.log(AWS_SECRET_KEY);
-    console.log(S3_BUCKET);
-
 });
 
 app.use('/uploads', express.static('./uploads/'));
 
-app.use(multer({ dest: './uploads/',
+app.use(multer({
+    dest: './uploads/',
     rename: function (fieldname, filename) {
         return filename;
     },
@@ -54,43 +58,28 @@ app.use(multer({ dest: './uploads/',
 app.use(bodyParser.json());
 
 app.post('/upload', function (req, res) {
-   console.log ('a');
+    console.log('a');
     console.log(JSON.stringify(req.body));
-    //console.log(__dirname);
-    res.end();
-});
-
-app.get('/test', function (req,res) {
-    console.log(S3_BUCKET);
-
-});
-
-app.get('/sign_s3', function(req, res){
-    console.log(JSON.stringify(req.query));
-    console.log('S3_BUCKET >' +  S3_BUCKET);
-
     aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-    var s3 = new aws.S3();
-    var s3_params = {
-        Bucket: S3_BUCKET,
-        Key: req.query.s3_object_name,
-        Expires: 60,
-        ContentType: req.query.s3_object_type,
-        //ContentType: req.query.s3_object_type,
-        ACL: 'public-read'
-    };
-    s3.getSignedUrl('putObject', s3_params, function(err, data){
-        if(err){
-            console.log(err);
-        }
-        else{
-            var return_data = {
-                signed_request: data,
-                url: 'http://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
-            };
-            //console.log
-            res.write(JSON.stringify(return_data));
-            res.end( );
+
+    var body = fs.createReadStream(req.files.upl.path).pipe(zlib.createGzip());
+    var s3obj = new aws.S3({
+        params: {
+            Bucket: S3_BUCKET,
+            Key: req.files.upl.name
         }
     });
+    s3obj.upload({Body: body}).
+        on('httpUploadProgress', function (evt) {
+            console.log(evt);
+        }).
+        send(function (err, data) {
+            console.log(err, data);
+            res.send({error:err,data:data});
+        });
 });
+
+app.get('/test', function (req, res) {
+    console.log(S3_BUCKET);
+});
+
