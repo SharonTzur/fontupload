b245ff2c11e4f267319905ea54fe3fdead40761f
Oren Griffin <oren.griffin@gmail.com>
2015-04-19 11:53:42 +0300

    intial comit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var multer = require('multer');
 var fs = require('fs');
-
+var aws = require('aws-sdk');
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -55,11 +55,37 @@ app.use(bodyParser.json());
 
 app.post('/upload', function (req, res) {
    console.log ('a');
-    console.log(__dirname);
+    console.log(JSON.stringify(req.body));
+    //console.log(__dirname);
     res.end();
 });
 
 app.get('/test', function (req,res) {
     console.log(S3_BUCKET);
 
+});
+
+app.get('/sign_s3', function(req, res){
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.s3_object_name,
+        Expires: 60,
+        ContentType: req.query.s3_object_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
 });
