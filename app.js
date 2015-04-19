1ff730552ce421c68cafa5c756f8530f9f6818be
Oren Griffin <oren.griffin@gmail.com>
2015-04-19 13:07:57 +0300

    intial comit

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ app.get('/sign_s3', function(req, res){
         Key: req.query.s3_object_name,
         Expires: 60,
         ContentType: req.query.s3_object_type,
+        //ContentType: req.query.s3_object_type,
         ACL: 'public-read'
     };
     s3.getSignedUrl('putObject', s3_params, function(err, data){
@@ -85,8 +86,9 @@ app.get('/sign_s3', function(req, res){
         else{
             var return_data = {
                 signed_request: data,
-                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
+                url: 'http://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
             };
+            //console.log
             res.write(JSON.stringify(return_data));
             res.end();
         }
