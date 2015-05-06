708391d94de862702a836139c9d8d1fdabdac611
Oren Griffin <oren.griffin@gmail.com>
2015-05-06 13:56:16 +0300

    heroku

--- globals.js
+++ globals.js
@@ -9,19 +9,14 @@ module.exports = {
     AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
     S3_BUCKET     : process.env.S3_BUCKET,
     uristring     : process.env.MONGOLAB_URI,
-    WIX_APP_SECRETX: process.env.WIX_APP_SECRETX,
-    WIX_APP_SECRETO: process.env.WIX_APP_SECRETO,
-    WIX_APP_IDX    : process.env.WIX_APP_IDX,
-    WIX_APP_IDO    : process.env.WIX_APP_IDO,
-
-    wix: {
-        WIX_APP_IDO: process.env.WIX_APP_SECRETO,
-        WIX_APP_IDX: process.env.WIX_APP_SECRETX
-    },
 
 
     init: function (mongoose) {
         var self = this;
+
+        self.wix[process.env.WIX_APP_IDO] = process.env.WIX_APP_SECRETO;
+        self.wix[process.env.WIX_APP_IDX] = process.env.WIX_APP_SECRETX;
+
         fs.readFile(__dirname + '/credentials.json', function (err, data) {
             if (data) {
                 var credentials = JSON.parse(data.toString());
@@ -29,8 +24,6 @@ module.exports = {
                 self.AWS_SECRET_KEY = credentials.secret_key;
                 self.S3_BUCKET = credentials.bucket;
                 self.uristring = credentials.mongo;
-                //self.WIX_APP_SECRET = credentials.wix_secret;
-                //self.WIX_APP_ID = credentials.wix_app_id;
                 self.wix[credentials.wix_app_id] = credentials.wix_secret;
             }
             console.log('wix creds are: ' +  JSON.stringify(self.wix));
