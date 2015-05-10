269513c89522ff95517da962565e77090c4cd117
Oren Griffin <oren.griffin@gmail.com>
2015-05-10 15:47:28 +0300

    delete dashboard

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 //    routers
-r.route(app, ['upload', 'load', 'save', 'test', 'webhook', 'dashboard',
+r.route(app, ['upload', 'load', 'save', 'test', 'webhook',
     ['settings', 'widgetAndSettings'],
     ['widget', 'widgetAndSettings']]);
 
