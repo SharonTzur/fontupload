9e219627ff7ae194b9c359df78226035a6e955a0
Rotem Meiri <rotemx@gmail.com>
2015-06-10 13:55:32 +0300

    changed font browser window added wix popup after upload

--- public/DB.js
+++ public/DB.js
@@ -48,15 +48,6 @@ function loadSettings(data) {
     var successFunc = function (data, status, jqXHR) {
         {
             widgetSettings = data;
-            widgetSettings.categoryDisplaySettings = {
-                All        : "true",
-                Serif      : "false",
-                SansSerif  : "false",
-                Monospace  : "false",
-                Handwriting: "false",
-                Display    : "false"
-            };
-            //console.log('Settings : load status: ' + status + '. Data :' + JSON.stringify(widgetSettings));
             refreshUI();
             refreshFontList();
             bindEvents();
